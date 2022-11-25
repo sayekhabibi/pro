58eee45717dff4cbc976e4bfd739890a795d08de
sayekhabibi <119029791+sayekhabibi@users.noreply.github.com>
2022-11-25 22:23:49 +0600

    Update MainActivity2.java

--- app/src/main/java/com/example/car/MainActivity2.java
+++ app/src/main/java/com/example/car/MainActivity2.java
@@ -34,8 +34,37 @@ public class MainActivity2 extends AppCompatActivity {
                 String number=et4.getText().toString();
 
 
+            Boolean i = g.insert_data(name, car_model, date, number);
+                if (i == true) {
+                    Toast.makeText(MainActivity2.this, "Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity2.this, "UnSuccessful", Toast.LENGTH_SHORT).show();
+                }
+
             }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor t=g.getinfo();
+                StringBuffer buffer=new StringBuffer();
+                while (t.moveToNext()) {
+                    buffer.append("Name:" + t.getString(1) + "\n");
+                    buffer.append("Car_model:" + t.getString(2) + "\n");
+                    buffer.append("Date:" + t.getString(3) + "\n");
+                    buffer.append("Number:" + t.getString(4) + "\n\n\n");
+                }
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity2.this);
+                builder.setCancelable(true);
+                builder.setTitle("Car Rental");
+                builder.setMessage(buffer.toString());
+                builder.show();
+
+            }
+
+
         });
 
     }
 }
+
